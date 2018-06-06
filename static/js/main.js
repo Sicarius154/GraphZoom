382e502fb70c4a52c60eea7954e8aff8411ec449
Chris <chris.jl154@outlook.com>
2018-06-06 15:43:38 +0100

    Trying to get labels working with nodes. Labels are drawn onto nodes but are not updated when changed.

--- static/js/main.js
+++ static/js/main.js
@@ -11,10 +11,12 @@ var cy = null;
 */
 function start(){
   //create the cytoscape objct and set the div with the corresponding ID to be the display area for the graph
-  cy = cytoscape({container: document.getElementById('graph-display-area'), style: [{selector: 'node', style :{"text-valign": "center","text-halign": "center", "label": "data(id)"}}, {selector: 'edge', style:{'curve-style': 'bezier'}}]});
+  cy = cytoscape({container: document.getElementById('graph-display-area'), style: [{selector: 'node', style :{"text-valign": "center","text-halign": "center", "label": "data(label)"}}, {selector: 'edge', style:{'curve-style': 'bezier'}}]});
 
-  //Set a function to handle when a node is selected
+  //Add event handlers
   cy.on('tap', 'node', nodeSelectedEvt);
+  cy.on('tap', 'edge', edgeSelectedEvt);
+  var area = document.getElementById("labelInputArea").addEventListener('input', labelAreaChangedEvt, false);
 }
 
 
@@ -22,20 +24,31 @@ function start(){
   This is called once a node has been selected. It displays the information of the most
   recently selected node
 */
-function showSelectedNodeData(){
-  selectedNodes = cy.$('node:selected');
-  node = selectedNodes[0];
+function showSelectedNodeData(node){
   //Update the information part of the information panel
-  document.getElementById("InformationArea").innerHTML =
-  "Node ID: " + node.id() + "<br>" + "Is connected to: ";
-  console.log("Node selected with ID: " + node.id());
+  document.getElementById("idArea").innerHTML = node.id();
+  document.getElementById("labelInputArea").value =  node.data.label;
+  console.log("Displaying element data with label " + node.data.label);
+  console.log(node);
+
+}
+
+/*
+  This is called once an edge has been selected. It displays the information of the most
+  recently selected edge
+*/
+function showSelectedEdgeData(edge){
+  //Update the information part of the information panel
+  document.getElementById("idArea").innerHTML = edge.id();
+  document.getElementById("labelInputArea").value =  edge.data.label;
+  console.log("Displaying element data with label " + edge.data.label);
 }
 
 /*
   Adds a node to the graph with a random ID number. It then selects the new node
 */
 function addNode(){
-  var node = {data:{id: 'n' + nextNodeId}, position:{x:0, y:0}};
+  var node = {data:{id: 'n' + nextNodeId, label: "No Label"}, position:{x:0, y:0}};
   cy.add(node);
 
   //Select the new node and increment the next ID available
@@ -71,32 +84,36 @@ function deleteElement(){
 }
 
 /*
-  Called when an ID of a node or edge is changed to ensure no other element has the same ID
-*/
-function isIdUnique(id){
-  for(element in cy.$('node')){
-    if(element.data.id == id){
-      return false;
-    }
-  }
-  for(element in cy.$('edge')){
-    if(element.data.id == id){
-      return false;
-    }
-  }
-  return true;
-}
-
-/*
   Assigns a new ID to the element
 */
-function assignId(currentID, newID){
-  cy.getElementById(currentID).data.id = newID;
+function assignLabel(id, newLabel){
+  cy.$('#' + id).data.label = newLabel;
+  console.log("New label assigned for element with ID: " + id + cy.$('#' + id).data.label);
 }
 
 /*
-  Called when a node is tapped/clicked. This will select the node and unselect any previous nodes that were selected.
+  Called when a node is tapped/clicked. This will select the node
 */
 function nodeSelectedEvt(evt){
-//  showSelectedNodeData();
+  console.log("Tapped on " + evt.target.id());
+  showSelectedNodeData(cy.$('#' + evt.target.id()));
+}
+
+/*
+  Called when a edge is tapped/clicked. This will select the edge
+*/
+function edgeSelectedEvt(evt){
+  console.log("Tapped on " + evt.target.id());
+  showSelectedEdgeData(cy.$('#' + evt.target.id()));
+}
+
+/*
+  Called when the textarea showing label information has its value changed
+*/
+function labelAreaChangedEvt(){
+  var id = document.getElementById("idArea").innerHTML;
+  var label = document.getElementById("labelInputArea").value;
+  console.log("Label found: " + label);
+  assignLabel(id, label);
+
 }
