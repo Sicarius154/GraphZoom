53f13b7c91e1d687d8f6bdaa54b026da1a8fdeb7
Chris <chris.jl154@outlook.com>
2018-07-16 10:34:45 +0100

    Changed the socket events ti have a more meaningful structure

--- server.py
+++ server.py
@@ -8,9 +8,7 @@ from flask_socketio import SocketIO
 import os
 from graph import Graph
 import datetime
-import pickle
-import tkinter
-from tkinter import filedialog
+
 script_dir = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'key123'
@@ -28,7 +26,7 @@ def root():
     print("Main UI requested. Rendering...")
     return render_template("index.html")
 
-@socketio.on('set_graph_data')
+@socketio.on('server_set_graph_data')
 def set_graph_data(json):
     '''
         This will set the elements of the graph object to the new values from the UIself.
@@ -39,30 +37,30 @@ def set_graph_data(json):
     graph.set_graph_from_json(json)
     print("Graph data sent to the front-end")
 
-@socketio.on('get_graph_data')
+@socketio.on('server_get_graph_data')
 def get_graph_data():
     '''
         This should send a JSON object representing the entire graph to the client-side
         It should emit the correct event. After this has been sent the UI is free to display the graph
     '''
     print("Sending requested graph data")
-    socketio.emit('new_graph_data', graph.getJSONRepresentation(), json=True)
+    socketio.emit('ui_set_graph_data', graph.getJSONRepresentation(), json=True)
 
-@socketio.on('connect')
+@socketio.on('server_connect')
 def connect():
     '''
         This shows that the client-side has now connected to the server
     '''
     print("Client connected to the server!")
 
-@socketio.on('disconnect')
+@socketio.on('server_disconnect')
 def disconnect():
     '''
         A client terminated the connection
     '''
     print("Client disconnected from the server!")
 
-@socketio.on('shutdown')
+@socketio.on('server_shutdown')
 def shutdown():
     '''
         Shut down the server, Save the current graph as an auto-save and terminate Flask
@@ -71,7 +69,7 @@ def shutdown():
     #TODO: Implement saving functionality
     os._exit(0)
 
-@socketio.on('save_new_relation')
+@socketio.on('server_save_new_relation')
 def save_new_relation(json):
     '''
         Set the relation data from a JSON string
@@ -81,16 +79,16 @@ def save_new_relation(json):
     graph.add_relation_from_json(json)
     print("New relation set for the current graph")
 
-@socketio.on('get_results_of_operation')
+@socketio.on('server_get_results_of_operation')
 def get_results_of_operation():
         '''
             Returns a graph as JSON to the front end that is supposed to represent the results of an operation(dilation, erosion etc) on the main graph.
         '''
         print("Front-end requested results of the previous operation to display (requested by new window displaying graphresult.html).")
-        socketio.emit("received_results_of_operation", graph.get_operation_results_as_json())
+        socketio.emit("ui_set_results_of_operation", graph.get_operation_results_as_json())
         print("Data sent to the front-end")
 
-@socketio.on("save_graph")
+@socketio.on("server_save_graph")
 def save_graph(path):
     '''
         Saves the graph to the 'saved' folder in the same relative folder as this script.
@@ -105,7 +103,7 @@ def save_graph(path):
     path = os.path.normpath(path)
     graph.save_graph(path)
 
-@socketio.on("load_graph")
+@socketio.on("server_load_graph")
 def load_graph(path):
     path = "\\saved\\" + path
     #Ensure the filename has the correct file extension
@@ -115,6 +113,14 @@ def load_graph(path):
     path = os.path.normpath(path)
     graph.load_graph(os.path.normpath(path))
     get_graph_data() #update the front end using the pre-configured function to update the UI with a new graph
+
+@socketio.on("set_subgraph_data")
+def set_subgraph_data(json):
+    graph.sub_graph.set_graph_from_json(json)
+
+@socketio.on("server_get_subgraph_data")
+def get_subgraph_data():
+    socket.
 #Start the application
 if __name__ == "__main__":
     socketio.run(app)
